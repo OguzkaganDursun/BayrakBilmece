5b32d71f0f4b1828cf31b34d2ec92ac63e04a683
Oğuzkağan Dursun <oguzkagandursun@outlook.com>
2020-05-01 16:52:23 +0300

    Formlar Olusturuldu.

--- BayrakBilmece/Cevap.cs
+++ BayrakBilmece/Cevap.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BayrakBilmece
 {
-    public partial class Form1 : Form
+    public partial class Cevap : Form
     {
-        public Form1()
+        public Cevap()
         {
             InitializeComponent();
         }
