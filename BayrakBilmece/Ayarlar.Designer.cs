007daff43e172ea5c93583d20fe5f6083c8dcef8
Oğuzkağan Dursun <oguzkagandursun@outlook.com>
2020-05-01 17:36:58 +0300

    Ayarlar formu değiştirildi

--- BayrakBilmece/Ayarlar.Designer.cs
+++ BayrakBilmece/Ayarlar.Designer.cs
@@ -34,8 +34,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(294, 270);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(784, 461);
             this.Name = "Ayarlar";
             this.Text = "Ayarlar";
             this.ResumeLayout(false);
