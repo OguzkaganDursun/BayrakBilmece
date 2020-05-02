5a6e3e398ce799c2c40997f4e967a91713dd9be6
Oğuzkağan Dursun <oguzkagandursun@outlook.com>
2020-05-02 03:24:56 +0300

    Uyari formu dizayn güncellemesi final

--- BayrakBilmece/Uyari.Designer.cs
+++ BayrakBilmece/Uyari.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(45, 9);
+            this.label1.Location = new System.Drawing.Point(41, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(117, 34);
             this.label1.TabIndex = 1;
