c6c37756d20ce9a72df146b86c481f212ae6c845
Oğuzkağan Dursun <oguzkagandursun@outlook.com>
2020-05-02 03:10:34 +0300

    Cevap formu dizayn güncellemesi final

--- BayrakBilmece/Cevap.Designer.cs
+++ BayrakBilmece/Cevap.Designer.cs
@@ -80,7 +80,7 @@
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(27, 47);
+            this.pictureBox1.Location = new System.Drawing.Point(28, 47);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 90);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -92,7 +92,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.Color.ForestGreen;
-            this.label2.Location = new System.Drawing.Point(39, 16);
+            this.label2.Location = new System.Drawing.Point(37, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(132, 22);
             this.label2.TabIndex = 2;
