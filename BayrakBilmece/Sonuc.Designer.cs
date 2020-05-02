a6729d37b572438c830f314d31ec50ac9f2bf90e
Oğuzkağan Dursun <oguzkagandursun@outlook.com>
2020-05-02 03:23:40 +0300

    Sonuc formu dizayn güncellemesi final v2

--- BayrakBilmece/Sonuc.Designer.cs
+++ BayrakBilmece/Sonuc.Designer.cs
@@ -77,7 +77,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.ForeColor = System.Drawing.Color.ForestGreen;
-            this.label3.Location = new System.Drawing.Point(66, 10);
+            this.label3.Location = new System.Drawing.Point(69, 10);
             this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 19);
