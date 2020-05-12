cc1c84eee1c15f0422ed4f6c87b1f9e3e7613fca
Oğuzkağan Dursun <oguzkagandursun@outlook.com>
2020-05-12 17:02:12 +0300

    Düzenleme yapıldı

--- BayrakBilmece/Hakkinda.cs
+++ BayrakBilmece/Hakkinda.cs
@@ -1,27 +1,26 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-//namespace BayrakBilmece
-//{
-//    public partial class Hakkinda : Form
-//    {
-//        public Hakkinda()
-//        {
-//            InitializeComponent();
-//        }
-
-//        private void button1_Click(object sender, EventArgs e)
-//        {
-//            if (Giris.ayarlar.oyunSesi == true)
-//                Giris.anaMenu.ButonMuzigiCal();
-//            this.Close();
-//        }
-//    }
-//}
+namespace BayrakBilmece
+{
+    public partial class Hakkinda : Form
+    {
+        public Hakkinda()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Giris.ayarlar.oyunSesi == true)
+                Giris.anaMenu.ButonMuzigiCal();
+            this.Close();
+        }
+    }
+}
